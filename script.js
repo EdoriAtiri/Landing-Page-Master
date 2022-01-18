3458df0f45e14a3a75813b0cbc3df6836de01b91
Edori Atiri <59481349+EdoriAtiri@users.noreply.github.com>
2022-01-18 12:23:02 +0100

    change font color to have better contrast, link styling ...

--- script.js
+++ script.js
@@ -5,34 +5,10 @@ const form = document.getElementById('form'),
   closeMenu = document.getElementById('menu-close'),
   nav = document.getElementById('navbar');
 
-import Glide, {
-  Controls,
-  Breakpoints,
-  Autoplay,
-  Keyboard,
-} from '@glidejs/glide/dist/glide.modular.esm';
-
-new Glide('.glide', {
-  type: 'carousel',
-  autoplay: 2500,
-  startAt: 0,
-  focusAt: 1,
-  perView: 3,
-  hoverpause: true,
-  keyboard: true,
-  breakpoints: {
-    1000: {
-      perView: 2,
-    },
-    800: {
-      perView: 1,
-    },
-  },
-}).mount({ Controls, Breakpoints, Autoplay, Keyboard });
-
 // check email is valid
 function checkEmail(input) {
-  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  const re =
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   if (re.test(input.value.trim())) {
     warning.style.opacity = 0;
     email.style.borderColor = 'limegreen';
