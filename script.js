eba57a37b98136264453d25aa2780ba55c81fe2e
Edori Atiri <59481349+EdoriAtiri@users.noreply.github.com>
2022-01-18 13:07:30 +0100

    adjusted menu button

--- script.js
+++ script.js
@@ -5,31 +5,6 @@ const form = document.getElementById('form'),
   closeMenu = document.getElementById('menu-close'),
   nav = document.getElementById('navbar');
 
-import Glide, {
-  Controls,
-  Breakpoints,
-  Autoplay,
-  Keyboard,
-} from '@glidejs/glide/dist/glide.modular.esm';
-
-new Glide('.glide', {
-  type: 'carousel',
-  autoplay: 2500,
-  startAt: 0,
-  focusAt: 1,
-  perView: 3,
-  hoverpause: true,
-  keyboard: true,
-  breakpoints: {
-    1000: {
-      perView: 2,
-    },
-    800: {
-      perView: 1,
-    },
-  },
-}).mount({ Controls, Breakpoints, Autoplay, Keyboard });
-
 // check email is valid
 function checkEmail(input) {
   const re =
@@ -54,14 +29,14 @@ function checkEmail(input) {
 // Open Nav Bar
 function slideOut() {
   openMenu.style.display = 'none';
-  closeMenu.style.display = 'inherit';
+  closeMenu.style.display = 'block';
   nav.classList.add('flyIn');
 }
 
 // Close Nav Bar
 function slideAway() {
   closeMenu.style.display = 'none';
-  openMenu.style.display = 'inherit';
+  openMenu.style.display = 'block';
   nav.classList.remove('flyIn');
 }
 
